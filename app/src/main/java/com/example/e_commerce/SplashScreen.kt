7123a69033c35da200d0e434c579dccd9e521349
mohanddo <93376862+mohanddo@users.noreply.github.com>
2024-07-29 21:21:48 +0100

    Adding splash screen

---
+++ app/src/main/java/com/example/e_commerce/SplashScreen.kt
@@ -0,0 +1,31 @@
+package com.example.e_commerce
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val splashScreenDuration = 3000L
+
+        Handler().postDelayed({
+            // Start main activity after the duration
+            startActivity(Intent(this, MainActivity::class.java))
+            // Finish the splash activity so that the user can't go back to it
+            finish()
+        }, splashScreenDuration)
+    }
+}
