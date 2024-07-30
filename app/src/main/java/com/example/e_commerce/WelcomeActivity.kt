60235b129411270d2980f87cd0d84482987befca
mohanddo <93376862+mohanddo@users.noreply.github.com>
2024-07-30 02:55:50 +0100

    Added the Welcome activity

--- app/src/main/java/com/example/e_commerce/WelcomeActivity.kt
+++ app/src/main/java/com/example/e_commerce/WelcomeActivity.kt
@@ -1,20 +1,33 @@
 package com.example.e_commerce
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.e_commerce.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var joinUs: Button
+    private lateinit var signIn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        joinUs = binding.joinus
+        joinUs.setOnClickListener {
+
+        }
+
     }
 }
